6e8942802d77121beb4324975eadbe0cd2d31b1f
Rafael Regis <rafael.regisp@gmail.com>
2010-08-26 16:48:46 -0300

    Adicionado TAD de vector e funcao de split

---
+++ vector.c
@@ -0,0 +1,91 @@
+#include "vector.h"
+
+void push_back(void* element, vector* v) {
+    if (v->size == v->max - 1) {
+        v->max += 10;
+        v->content = (void**)realloc(v->content, v->max*sizeof(void*));
+    }
+    v->content[v->size++] = element;
+}
+
+size_t size(vector* v) {
+    return v->size;
+}
+
+int empty(vector* v) {
+    return (v->size == 0);
+}
+
+void* element(vector* v, size_t pos) {
+    if (v->size <= pos) {
+        printf("ERRO: Acesso a posicao nao existente\n");
+        exit(1);
+    } else {
+        return v->content[pos];
+    }
+}
+
+void* front(vector* v) {
+    if (empty(v)) {
+        return NULL;
+    } else {
+        return v->content[0];
+    }
+}
+
+void* back(vector* v) {
+    if (empty(v)) {
+        return NULL;
+    } else {
+        return v->content[v->size - 1];
+    }
+}
+
+void insert(vector* v, size_t pos, void* el) {
+    if (v->size < pos) {
+        v->content[pos] = el;
+    } else {
+        printf("ERRO: Acesso a posicao nao existente\n");
+        exit(1);
+    }
+}
+
+void erase(vector* v, size_t pos, void* el) {
+    int i;
+    if (v->size < pos) {
+        free(v->content[pos]);
+        v->size--;
+        for (i = pos; i < v->size; i++)
+            v->content[i] = v->content[i+1];
+    } else {
+        printf("ERRO: Acesso a posicao nao existente\n");
+        exit(1);
+    }
+}
+
+vector* new_vector() {
+    int i;
+    
+    vector* v = (vector*)malloc(sizeof(vector));
+    v->content = (void**)realloc(NULL, 10*sizeof(void*));
+    v->max = 20;
+    v->size = 0;
+    
+    for (i = 0; i < v->max; i++)
+        v->content[i] = NULL;    
+    
+    return v;
+}
+ 
+void delete_vector(vector* v) {
+    int i;
+
+    if(v != NULL) {
+        for(i = 0; i < v->size; i++) {
+            if (v->content[i] != NULL) {
+                free(v->content[i]);
+            }
+        }
+        free(v);
+    }
+}
